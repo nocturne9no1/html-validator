0462329af19a74bdf7fc67d37c5f653781587df9
Nocturne9no1 <nocturne9no1@gmail.com>
2022-06-07 23:40:09 +0900

    Add: html validating module

--- html-validator/public/main.js
+++ html-validator/public/main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, shell } = require('electron');
 const { ipcMain } = require('electron');
 const remote = require('@electron/remote/main');
 const path = require('path');
 const { readFileSync } = require('original-fs');
+const fs = require('fs');
+const { execFile } = require('child_process');
+const vnu = require('vnu-jar');
 
 remote.initialize();
 
@@ -23,7 +26,14 @@ function createWindow() {
   remote.enable(win.webContents);
 }
 
-app.on('ready', createWindow);
+app.on('ready', function() {
+  createWindow();
+  const homedir = require('os').homedir();
+  const homeDirList = fs.readdir(homedir, function(err, data) {
+    console.log(data);
+  })
+  console.log(homeDirList);
+});
 
 app.on('window-all-closed', function() {
   if(process.platform !== 'darwin') {
@@ -33,25 +43,29 @@ app.on('window-all-closed', function() {
 
 app.on('activate', function() {
   if(BrowserWindow.getAllWindows().length === 0) createWindow();
+
 })
 
 // ipc communication
 ipcMain.on('CHANNEL_NAME', (evt, payload) => {
   console.log(payload);
-  (async () => {
-    const validator = require('html-validator')
-    const options = {
-      url: 'http://google.com',
-      format: 'text',
-      // data: readFileSync('./index.html', 'utf8')
-    }
-    
-    try {
-      const result = await validator(options)
-      console.log(result)
-    } catch (error) {
-      console.error(error)
-    }
-  })()
+
   evt.reply('IPC_RENDERER_CHANNEL_NAME', 'message');
+  fs.readdir('./public', function(err, data) {
+    console.log(data);
+  })
+  const currentDir = __dirname;
+  const test = path.join(currentDir, '/test.html');
+
+  console.log('hi its vnu', vnu);
+
+  // Work with vnu.jar, for example get vnu.jar version
+  execFile('java', ['-jar', `"${vnu}"`, test], { shell: true }, (error, stdout) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log('실행이 됬다', stdout);
+    console.log(stdout);
+  });
 })
